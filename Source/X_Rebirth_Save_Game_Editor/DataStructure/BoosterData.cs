550f040ea53949f473606a68b9131f0da315f49d
Duploli <olivierlantsoght@hotmail.com>
2017-02-10 23:17:03 +0100

    Value are correctly read whatever the system language (doc or coma decimal). Need to change time to double precision

--- Source/X_Rebirth_Save_Game_Editor/DataStructure/BoosterData.cs
+++ Source/X_Rebirth_Save_Game_Editor/DataStructure/BoosterData.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 using X_Rebirth_Save_Game_Editor.Helper;
+using X_Rebirth_Save_Game_Editor.Logging;
 
 namespace X_Rebirth_Save_Game_Editor.DataStructure
 {
@@ -34,12 +35,12 @@ namespace X_Rebirth_Save_Game_Editor.DataStructure
             BoosterNode.Attributes.Append(parent.OwnerDocument.CreateAttribute("faction"));
             BoosterNode.Attributes["faction"].Value = faction;
             BoosterNode.Attributes.Append(parent.OwnerDocument.CreateAttribute("relation"));
-            BoosterNode.Attributes["relation"].Value = relation.ToString();
+            BoosterNode.Attributes["relation"].Value = XmlConvert.ToString(relation);
             // Max/min value observed in game save
             // 0.15 for a discount booster
             // 0.578817/-1  for a relation booster
             BoosterNode.Attributes.Append(parent.OwnerDocument.CreateAttribute("time"));
-            BoosterNode.Attributes["time"].Value = time.ToString();
+            BoosterNode.Attributes["time"].Value = XmlConvert.ToString(time);
             // Usual time in savegame: ~150e3 (not expressed in scientific notation)
             parent.AppendChild(BoosterNode);
         }
@@ -58,16 +59,14 @@ namespace X_Rebirth_Save_Game_Editor.DataStructure
         {
             get
             {
-                float f;
-                if (float.TryParse(XMLFunctions.GetSafeAttribute(BoosterNode, "relation"), out f))
-                {
-                    return f;
-                }
-                return 0;
+                float f = XmlConvert.ToSingle(XMLFunctions.GetSafeAttribute(BoosterNode, "relation"));
+                // Using XmlConvert.ToSingle to convert with the decimal separator used in xml document (dot)
+                return f;
             }
             set
             {
-                XMLFunctions.SetSafeAttribute(BoosterNode, "relation", value.ToString());
+                XMLFunctions.SetSafeAttribute(BoosterNode, "relation", XmlConvert.ToString(value));
+                // Using XmlConvert.ToString to convert the value with the dot decimal separator whatever was set in the GUI
             }
         }
 
@@ -75,16 +74,12 @@ namespace X_Rebirth_Save_Game_Editor.DataStructure
         {
             get
             {
-                float f;
-                if (float.TryParse(XMLFunctions.GetSafeAttribute(BoosterNode, "time"), out f))
-                {
-                    return f;
-                }
-                return 0;
+                float f = XmlConvert.ToSingle(XMLFunctions.GetSafeAttribute(BoosterNode, "time"));
+                return f;
             }
             set
             {
-                XMLFunctions.SetSafeAttribute(BoosterNode, "time", value.ToString());
+                XMLFunctions.SetSafeAttribute(BoosterNode, "time", XmlConvert.ToString(value));
             }
         }
         #endregion
